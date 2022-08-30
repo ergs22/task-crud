74f6f66c54b376790ff872014bec5d3d32575319
ergs22 <ernestgarcia1122@gmail.com>
2022-08-30 16:37:57 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import app from "./app";
 import "./database";
-import { PORT } from "./config";
 
-app.listen(PORT);
-console.log("server on port", PORT);
+app.listen(app.get("port"));
+console.log(`server on port ${app.get("port")}`);
